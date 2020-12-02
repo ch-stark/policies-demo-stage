3a1cec82f2be292f576faa8bcb87de53410f47bc
Christian Stark <cstark@cstark.muc.csb>
2020-12-02 14:17:30 +0100

    set to inform

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+FILES=policies/*
+for f in $FILES
+do
+  if (! grep -q notenforce "$f")
+   then
+     echo "replace inform with enforce in" $f
+     sed -i "s/enforce/inform/g" $f
+  else
+     echo "not replace inform with enforce in" $f
+  fi
+done
